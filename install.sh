007fbde5542625d3ebe0b8373c273eaea654e400
sanine-a <sanine.not@pm.me>
2019-11-27 10:25:21 -0600

    add install script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#get current directory
+CAMERA_DIR=`pwd`
+
+#install dependencies
+sudo apt install build-essential cmake libgtk-3-dev libopencv-dev libserialport-dev pkg-config
+
+#prepare makefile
+cmake .
+
+#build
+make
+
+#provide startup scripts
+echo "cd $CAMERA_DIR/bin
+./config" > $HOME/bin/config.sh
+
+echo "cd $CAMERA_DIR/bin
+./rollercoaster" > $HOME/bin/rollercoaster.sh
+
+chmod 740 $HOME/bin/config.sh
+chmod 740 $HOME/bin/rollercoaster.sh
+
+#provide desktop shortcuts
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Exec=$HOME/bin/config.sh
+Name=Configurator
+Comment=
+Icon=$CAMERA_DIR/bin/icon.png" > $HOME/Desktop/Configurator.desktop
+
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=true
+Exec=$HOME/bin/rollercoaster.sh
+Name=Coaster
+Comment=
+Icon=$CAMERA_DIR/bin/icon.png" > $HOME/Desktop/Coaster.desktop
+
+# mark desktop shortcuts executable
+chmod 740 $HOME/Desktop/Configurator.desktop
+chmod 740 $HOME/Desktop/Coaster.desktop
+
+# set desktop shortcuts as trusted
+#gio set $HOME/Desktop/Configurator.desktop "metadata::trusted" yes
+#gio set $HOME/Desktop/Coaster.desktop "metadata::trusted" yes
